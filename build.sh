f2c4eb74d7d64b9f655bcea9bc8cb9ae50280617
vkobinski <victorkobinski@Outlook.com>
2022-01-17 17:47:39 -0300

    first commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+g++ -o RayCast main.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
---
+++ main.cpp
@@ -0,0 +1,111 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+#include <vector>
+#include <iostream>
+
+class RayCast : public olc::PixelGameEngine
+{
+public:
+	RayCast()
+	{
+		sAppName = "Ray Cast";
+	}
+
+	olc::vf2d vPlayer = { 0,0 };
+	olc::vi2d vMapSize = { 32,30 };
+	olc::vi2d vCellSize = { 16,16 };
+	std::vector<int> vecMap;
+	float fRayAngle;
+
+private:
+	void DrawBoard() {
+		for(int y = 0; y < vMapSize.y; y++)
+		{
+			for(int x = 0; x < vMapSize.x; x++)
+			{
+				int cell = vecMap[y * vMapSize.x + x];
+				if(cell == 1)
+					FillRect(olc::vi2d(x,y) * vCellSize, vCellSize, olc::BLUE);
+				DrawRect(olc::vi2d(x,y) * vCellSize, vCellSize, olc::DARK_GREY);
+			}
+		}
+	}
+
+private:
+	void DrawRay(float ray2, olc::vf2d vRayStart){
+			
+		float step_count = 0.0f;
+		bool hit = false;
+		float max = 20.0f;
+
+		float fEyeX = sin(ray2);
+		float fEyeY = cos(ray2);
+
+		while(!hit && step_count < max){
+			step_count += 0.1;
+
+			int nTestX = (int)(vRayStart.x + fEyeX * step_count);
+			int nTestY = (int)(vRayStart.y + fEyeY * step_count);
+
+			if(nTestX < 0 || nTestX >= vMapSize.x || nTestY < 0 || nTestY >= vMapSize.y){
+				DrawLine(vRayStart * vCellSize, olc::vi2d(nTestX,nTestY)*vCellSize, olc::YELLOW);
+				continue;
+			}else{
+ 				int cell = vecMap[nTestY * vMapSize.x + nTestX];
+				if(cell == 1){
+					DrawLine(vRayStart * vCellSize, olc::vi2d(nTestX,nTestY)*vCellSize, olc::YELLOW);
+					hit = true;
+				}
+			}
+		}
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		vecMap.resize(vMapSize.x * vMapSize.y);
+		fRayAngle = 0;
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		olc::vf2d vMouse = { float(GetMouseX()), float(GetMouseY()) };
+		olc::vf2d vMouseCell = vMouse / vCellSize;
+		olc::vi2d vCell = vMouseCell;
+
+		if(GetMouse(olc::Mouse::LEFT).bHeld) vecMap[vCell.y * vMapSize.x + vCell.x] = 1;
+
+		if(GetKey(olc::Key::W).bHeld) vPlayer.y -= 25.0f * fElapsedTime;
+		if(GetKey(olc::Key::S).bHeld) vPlayer.y += 25.0f * fElapsedTime;
+		if(GetKey(olc::Key::A).bHeld) vPlayer.x -= 25.0f * fElapsedTime;
+		if(GetKey(olc::Key::D).bHeld) vPlayer.x += 25.0f * fElapsedTime;
+
+		if(GetKey(olc::Key::RIGHT).bHeld) fRayAngle += 10.0f * fElapsedTime;
+
+		olc::vf2d vRayStart = vPlayer;
+
+		Clear(olc::BLACK);
+		DrawBoard();
+		for(float ray = fRayAngle - 2; ray < fRayAngle; ray += 0.01f){
+			DrawRay(ray, vRayStart);
+		}
+		
+		FillCircle(vPlayer * vCellSize, 4.0f, olc::RED);
+
+		DrawString({10,10}, "Angle: " + std::to_string(fRayAngle));
+		if(fRayAngle >= 360){
+			fRayAngle = 0;
+		}
+		
+		return true;
+	}
+};
+
+int main(){
+    RayCast rc;
+    if(rc.Construct(512,480,4,4))
+        rc.Start();
+
+    return 0;
+}
